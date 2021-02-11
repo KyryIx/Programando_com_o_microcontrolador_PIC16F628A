c2fb2ea181bec525a7c33a94ddd09c4612ae1aad
Everton P. Cruz <kyry.ix@gmail.com>
2021-02-10 21:02:56 -0300

    Update Código 5.9 - Exemplo do usa da instrução BTFSS f,d.asm

--- Código 5.9 - Exemplo do usa da instrução BTFSS f,d.asm
+++ Código 5.9 - Exemplo do usa da instrução BTFSS f,d.asm
@@ -1,11 +1,11 @@
 ; INSTRUCAO: BTFSS
 ; TESTA SE O BIT b DO REGISTRADOR f ESTA setado (valor 1)
 ; SE ESTIVER setado (valor 1) SALTA A PROXIMA INSTRUCAO
-MOVLW   .197      ; (1) 197 DECIMAL = 1100.0101 BINARIO
-MOVWF   0x0C      ; (2)
-                  ; (3)
-BTFSS   0x0C, 5   ; (4) BIT NA POSICAO 5 EH ZERO >--+
-                  ; (5)                             |
-NOP               ; (6) <---------------------------+
-                  ; (7)
-NOP               ; (8)
+MOVLW   .197      ; 197 DECIMAL = 1100.0101 BINARIO
+MOVWF   0x0C      ;
+                  ;
+BTFSS   0x0C, 5   ; BIT NA POSICAO 5 EH ZERO >--+
+                  ;                             |
+NOP               ; <---------------------------+
+                  ;
+NOP               ;
