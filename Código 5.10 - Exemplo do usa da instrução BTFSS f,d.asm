16c87889cca2bdb85e6c49be06df9bd5e0406c1d
Everton P. Cruz <kyry.ix@gmail.com>
2021-02-10 21:08:12 -0300

    Update Código 5.10 - Exemplo do usa da instrução BTFSS f,d.asm

--- Código 5.10 - Exemplo do usa da instrução BTFSS f,d.asm
+++ Código 5.10 - Exemplo do usa da instrução BTFSS f,d.asm
@@ -1,11 +1,11 @@
 ; INSTRUCAO: BTFSC
 ; TESTA SE O BIT b DO REGISTRADOR f ESTA setado (valor 1)
 ; SE ESTIVER setado (valor 1) SALTA A PROXIMA INSTRUCAO
-MOVLW   .197      ; (1) 197 DECIMAL = 1100.0101 BINARIO
-MOVWF   0x0C      ; (2)
-                  ; (3)
-BTFSS   0x0C, 2   ; (4) BIT NA POSICAO 2 EH UM >----+
-                  ; (5)                             |
-NOP               ; (6)                             |
-                  ; (7)                             |
-NOP               ; (8) <---------------------------+
+MOVLW   .197      ; 197 DECIMAL = 1100.0101 BINARIO
+MOVWF   0x0C      ;
+                  ;
+BTFSS   0x0C, 2   ; BIT NA POSICAO 2 EH UM >----+
+                  ;                             |
+NOP               ;                             |
+                  ;                             |
+NOP               ; <---------------------------+
