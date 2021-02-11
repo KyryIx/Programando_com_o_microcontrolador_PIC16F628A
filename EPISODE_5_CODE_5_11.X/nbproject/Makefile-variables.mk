a81a93f3fed6ec22a57c223dcd70410165aa1fe8
Everton P. Cruz <kyry.ix@gmail.com>
2021-02-10 23:44:21 -0300

    Complete code using example 5.11 in episode 5

---
+++ EPISODE_5_CODE_5_11.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=EPISODE_5_CODE_5_11.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/EPISODE_5_CODE_5_11.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=episode5code511.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/episode5code511.x.tar
